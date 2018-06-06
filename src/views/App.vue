4f5b8a19f218f1723a2c189daf2ad255b55e602d
ccy <1292572728@qq.com>
2018-06-07 00:52:32 +0800

    加上转场动画

--- src/views/App.vue
+++ src/views/App.vue
@@ -1,7 +1,19 @@
 <template lang="html">
   <div id="application">
+    <transition :enter-active-class="transactionEnterLiveName"
+                :leave-active-class="transactionLeaveLiveName"
+    >
     <router-view></router-view>
+    </transition>
   </div>
 </template>
 <script>
+  export default {
+    data(){
+      return {
+        transactionEnterLiveName:"animated-slow fadeIn",
+        transactionLeaveLiveName:"animated-slow fadeOut",
+      }
+  }
+  }
 </script>
